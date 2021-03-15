3787ed0b1b4dddf2ddcb98b37a56b7db5949f3d7
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-15 19:25:05 +0530

    Imported Headr Component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <h1>Rishav</h1>
+      <Header dark={true} className="pl-4">Header</Header>
     </div>
   );
 }
