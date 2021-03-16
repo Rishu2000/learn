0d0641294cb03f12d07c9486b38d74b20c42f9df
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-16 16:44:43 +0530

    fetch the data, setTimeout functionality has been added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, {useState, useEffect} from "react"
 import './App.css';
 import Header from "./components/Header"
 
 function App() {
+
+  const [students,setStudents] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+        fetch("data.json")
+        .then((res) => res.json())
+        .then((students) => setStudents(students));
+    },1000)
+  })
+
   return (
     <div className="App">
       <Header dark={true} className="pl-4">Header</Header>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <pre className="border rounded bg-light p-3">
+              {JSON.stringify(students,null,2)}
+            </pre>
+          </div>
+        </div>
+      </div>
+    </div>  
   );
 }
 
