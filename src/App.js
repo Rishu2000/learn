337f23d83942a5c877fa694ae4c4dbad07dac05d
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-16 17:03:45 +0530

    Implemented Routing funcionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
 import './App.css';
 import Header from "./components/Header"
+import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
 
@@ -15,18 +16,20 @@ function App() {
   })
 
   return (
-    <div className="App">
-      <Header dark={true} className="pl-4">Header</Header>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            {students.length > 0? <pre className="border rounded bg-light p-3">
-              {JSON.stringify(students,null,2)}
-            </pre>: "Loading..."}
+    <Router>
+      <div className="App">
+        <Header dark={true} className="pl-4">Header</Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {students.length > 0? <pre className="border rounded bg-light p-3">
+                {JSON.stringify(students,null,2)}
+              </pre>: "Loading..."}
+            </div>
           </div>
         </div>
-      </div>
-    </div>  
+      </div>  
+    </Router>
   );
 }
 
